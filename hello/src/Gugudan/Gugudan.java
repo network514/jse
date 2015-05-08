262fb8ef75b9818a9931e41a4bfc0f1de9f8351a
취업반 PM < PM@itbank-PC>
2015-05-08 21:37:14 +0900

    뭔가 추가

--- hello/src/Gugudan/Gugudan.java
+++ hello/src/Gugudan/Gugudan.java
@@ -2,6 +2,7 @@ package Gugudan;
 //패키지와 클래스 사이에는 엔터를 준다.
 public class Gugudan { // 메소드 사이에는 enter를 주지 않는다.
 	public static void main(String[] args) {
+		System.out.println("룰루랄라");
 		int k, l;
 		for (int i = 1; i < 19; i++) {
 			for (int j = 2; j < 6; j++) {
--- hello/src/Gugudan/lulu.java
+++ hello/src/Gugudan/lulu.java
@@ -1,10 +1,8 @@
 package Gugudan;
 
 public class lulu {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
 
 }
